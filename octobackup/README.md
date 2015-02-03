240837e435a238137a5802ea279cd714269d020a
skhaen <skhaen@cyphercat.eu>
2015-02-03 11:35:04 +0100

    relecture

--- octobackup/README.md
+++ octobackup/README.md
@@ -1,2 +1,12 @@
 # Script pour faire des sauvegardes facilement
 
+https://www.octopuce.fr/faire-des-backups-facilement/
+
+```bash
+wget https://github.com/octopuce/octopuce-goodies/blob/master/octobackup/backup.sh
+mv backup.sh /usr/local/bin
+chmod +x /usr/local/bin/backup.sh
+```
+
+Ne pas oublier de modifier DEST et HOST.
+
--- octobackup/backup.sh
+++ octobackup/backup.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
-# Parametres du backup : 
-
-DEST=/media/EXTERNAL/backup/
+# Paramètres du backup
+## Vers quel disque dur
+DEST=/media/octobackup/
+## Nombre de jours au-delà duquel on supprime les anciens backups
 DAYS=300
-# pour etre plus bavard, decommenter cette option : 
-RSYNC_OPTIONS="-P -H"
+## Nom de votre ordinateur (pratique si vous en avez plusieurs)
 HOST="laptop"
 
-# On backupe dans un dossier <host>_<date>
+RSYNC_OPTIONS="-P -H"
+if [ ! -d "$DEST" ]
+then
+  echo "Disque de sauvegarde non monté !"
+  exit 1
+fi
+# On backup dans un dossier <host>_<date>
 NOW="${HOST}_`date +%Y%m%d`"
 LINKDEST=""
 for i in `ls -rt $DEST|tail -3`
@@ -23,7 +29,6 @@ mkdir "$DEST/$NOW"
 rsync / $DEST/$NOW/ -a --delete --numeric-ids --exclude "proc/*" --exclude "sys/*" --exclude "dev/*" --exclude "home/skhaen/.gvfs" --exclude "media/*" --exclude "/mnt/" $LINKDEST $RSYNC_OPTIONS
 touch "$DEST/$NOW"
  
-# Ensuite on fait du menage : effacement des dossiers de plus de X jours
+# Ensuite on fait du ménage : effacement des dossiers de plus de X jours
 echo "Cleanup ..."
-find "$DEST" -maxdepth 1 -mindepth 1 -type d -mtime +$DAYS -print -exec rm -rf {} \; 
-
+find "$DEST" -maxdepth 1 -mindepth 1 -type d -mtime +$DAYS -print -exec rm -rf {} \;
