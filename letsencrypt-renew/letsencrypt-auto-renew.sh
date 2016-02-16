dd12c805c39f1903ffe17eea11339ba4ff753da6
Benjamin Sonntag <benjamin@octopuce.fr>
2016-02-16 07:55:38 +0100

    no need for a .ini + pwd fix + else

--- letsencrypt-renew/letsencrypt-auto-renew.sh
+++ letsencrypt-renew/letsencrypt-auto-renew.sh
@@ -6,7 +6,7 @@ LETSENCRYPT_BIN=/usr/local/letsencrypt/letsencrypt-auto
 WEBROOT=/var/www/letsencrypt 
 
 cd "$(dirname $0)" 
-HERE="$PWD "
+HERE="$PWD"
 DATE_TODAY=$(date +'%s') 
 cd /etc/letsencrypt/live || (echo "Can't cd to /etc/letsencrypt/live !" && exit -1) 
 for domain in * 
@@ -18,14 +18,19 @@ do
 	DATE_JOURS_DIFF=$(( ( $DATE_CERT - $DATE_TODAY ) / (60*60*24) )) 
 	if [[ $DATE_JOURS_DIFF -le 30 ]]; then 
 	    echo "Trying to renew certificate for domain $domain expiring in $DATE_JOURS_DIFF days" 
-		    # Read the SAN (Subject Alt Names) for this cert (Warn: this code may not be super reliable :/ ) 
+	    # Read the SAN (Subject Alt Names) for this cert (Warn: this code may not be super reliable :/ ) 
 	    SAN=$(openssl x509 -in "$CERT"  -text|grep DNS:|sed -e "s/DNS:/-d /g" -e "s/, / /g") 
-		        # Try to renew it: 
-	    $LETSENCRYPT_BIN certonly --config "$HERE/renew.ini" --webroot -w "$WEBROOT" -d "$domain" $SAN 
+	    # Try to renew it: 
+	    $LETSENCRYPT_BIN certonly --webroot -w "$WEBROOT" -d "$domain" $SAN 
 	    if [ "$?" -ne "0" ] 
 	    then 
 		echo "Certificate /etc/letsencrypt/live/$domain has NOT been successfully renewed, please check" | mail -s "Can't renew certificate $domain on $(hostname)" $ALERT 
-	    fi 
+	    else
+		# TODO : auto-reload of services ? 
+		echo "Renewed certificate $domain on $(hostname)"
+	    fi
+	    
 	fi 
     fi
 done 
+
