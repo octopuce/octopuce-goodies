6887ec83f3b14468b7b48b93e3392463158934b6
neodiablow <francois@octopuce.fr>
2018-07-04 16:51:25 +0200

    [add] script to "compile" scores for spamassassin

---
+++ spamassin-split/scoreMinGen.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import sys;
+import os;
+
+
+
+def compareRuleFiles (sourceFile,modifiedFile):
+    with open(sourceFile) as sf:
+        source_data=sf.readlines()
+        
+    source_data=[item.strip() for item in source_data]
+    sorted(source_data)
+    with open(modifiedFile) as sf:
+        modified_data=sf.readlines()
+        
+    modified_data=[item.strip() for item in modified_data]
+    sorted(modified_data)
+
+    for so_spam_lot in source_data :
+        for mo_spam_lot in modified_data :
+            if(so_spam_lot.split(",")[0]==mo_spam_lot.split(",")[0]):
+                if(so_spam_lot.split(",")[1]!=mo_spam_lot.split(",")[1]):
+                    print("score: " + mo_spam_lot.split(',')[0]+" "+mo_spam_lot.split(",")[1])
+
+
+compareRuleFiles(sys.argv[1],sys.argv[2])
